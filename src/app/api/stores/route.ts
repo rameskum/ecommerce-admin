1f40a1ade819937ab189783d3751eb9ecf60c277
Ramesh Kumar <rameskum.ms@outlook.com>
2024-03-13 15:36:29 -0400

    create store api implementation, axios dependency added

---
+++ src/app/api/stores/route.ts
@@ -0,0 +1,29 @@
+import prismadb from '@/lib/prismadb';
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request) {
+	try {
+		const { userId } = auth();
+		if (!userId) {
+			return new NextResponse('Unauthorized', { status: 401 });
+		}
+		const body = await req.json();
+		const { name } = body;
+		if (!name) {
+			new NextResponse('Name is required', { status: 400 });
+		}
+
+		const store = await prismadb.store.create({
+			data: {
+				name,
+				userId,
+			},
+		});
+
+		return NextResponse.json(store);
+	} catch (e) {
+		console.error('[STORES_POST]', e);
+		return new NextResponse('Internal Server Error', { status: 500 });
+	}
+}
