2d105f5e9d95eea91a3dbfde6e3e5c9200d4c684
rameskum <rameskum.ms@outlook.com>
2024-05-05 01:00:32 -0400

    reverting last change and adding Access-Control-Allow-Credentials header

--- src/app/api/[storeId]/checkout/route.ts
+++ src/app/api/[storeId]/checkout/route.ts
@@ -7,6 +7,8 @@ import { stripe } from '@/lib/stripe';
 const corsHeaders = {
 	'Access-Control-Allow-Origin': `${process.env.FRONTEND_STORE_URL!}`,
 	'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+	'Access-Control-Allow-Credentials': 'true',
 };
 
 export async function OPTIONS() {
