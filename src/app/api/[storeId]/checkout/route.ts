daa5a976eede17b715f3be58540f0d6f4a51b683
rameskum <rameskum.ms@outlook.com>
2024-05-05 00:37:59 -0400

    changed the allowed origins to support slash

--- src/app/api/[storeId]/checkout/route.ts
+++ src/app/api/[storeId]/checkout/route.ts
@@ -5,7 +5,8 @@ import prismadb from '@/lib/prismadb';
 import { stripe } from '@/lib/stripe';
 
 const corsHeaders = {
-	'Access-Control-Allow-Origin': process.env.FRONTEND_STORE_URL!,
+	'Access-Control-Allow-Origin': `${process.env.FRONTEND_STORE_URL!}/, ${process
+		.env.FRONTEND_STORE_URL!}`,
 	'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
 	'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 };
