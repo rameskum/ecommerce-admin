466fdbbcab9b76b3d39478eb4338e6f705c5df7b
rameskum <rameskum.ms@outlook.com>
2024-05-04 20:28:52 -0400

    Revert "adding dotenv"
    This reverts commit 4590fd73df5964faf0af93c38873cf144639dc34.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,19 @@
 import './globals.css';
 
-import * as dotenv from 'dotenv';
-
-import { ClerkProvider } from '@clerk/nextjs';
 import { Inter } from 'next/font/google';
-import type { Metadata } from 'next';
-import { ModalProvider } from '@/providers/modal-provider';
-import { ThemeProvider } from '@/components/theme-provider';
-import { ToasterProvider } from '@/providers/toast-provider';
 
+import { ModalProvider } from '@/providers/modal-provider';
+import { ToasterProvider } from '@/providers/toast-provider';
+import { ClerkProvider } from '@clerk/nextjs';
+
+import type { Metadata } from 'next';
+import { ThemeProvider } from '@/components/theme-provider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Admin Dashboard',
 	description: 'Admin Dashboard',
 };
-
-dotenv.config();
 
 export default function RootLayout({
 	children,
